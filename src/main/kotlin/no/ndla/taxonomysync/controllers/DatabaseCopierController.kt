bedff9b1b2180aa0827724810a23b921b081363c
Øystein Øvrebø <oystein.ovrebo@bouvet.no>
2019-03-15 11:52:27 +0100

    adresse til resetdraft > /database/resetdraft

--- src/main/kotlin/no/ndla/taxonomysync/controllers/DatabaseCopierController.kt
+++ src/main/kotlin/no/ndla/taxonomysync/controllers/DatabaseCopierController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 
-@Controller("/database")
+@Controller("database")
 class DatabaseCopierController(val databaseCopierService: DatabaseCopierService) {
 
 
