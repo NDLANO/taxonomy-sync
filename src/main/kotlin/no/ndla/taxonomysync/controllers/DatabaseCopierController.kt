ca79a4253fbbc516ef533d5d109144aa3a11f69c
Kristian Nøstdal <kristian.nostdal@bouvet.no>
2019-03-26 13:10:51 +0100

    Added swagger

--- src/main/kotlin/no/ndla/taxonomysync/controllers/DatabaseCopierController.kt
+++ src/main/kotlin/no/ndla/taxonomysync/controllers/DatabaseCopierController.kt
@@ -1,6 +1,6 @@
 package no.ndla.taxonomysync.controllers
 
-import no.ndla.taxonomysync.configurations.RequestQueueConfiguration
+import io.swagger.annotations.ApiOperation
 import no.ndla.taxonomysync.dtos.CopyReport
 import no.ndla.taxonomysync.services.DatabaseCopierService
 import no.ndla.taxonomysync.services.DynamoDbService
@@ -10,29 +10,21 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("database")
 class DatabaseCopierController(val databaseCopierService: DatabaseCopierService,
-                               val dynamoDbService: DynamoDbService,
-                               val config: RequestQueueConfiguration
-                               ) {
-
+                               val dynamoDbService: DynamoDbService) {
 
     @PostMapping("/resetdraft")
     @ResponseBody
+    @ApiOperation(value = "Resets the draft", notes = "Only use when you want to delete everything. This will overwrite the draft db with the target environment db")
     fun resetDraftDatabase(): CopyReport {
         dynamoDbService.resetTable()
         return databaseCopierService.copySourceToTarget()
     }
 
     @GetMapping("/insertmock")
+    @ApiOperation(value = "Insert a mock", notes = "Used for testing purposes.")
     fun makeStuff():CopyReport{
         //TODO remove after everything works. This is only for mocking purposes.
         return dynamoDbService.createTable()
-    }
-
-    @GetMapping
-    fun configTest() {
-        //TODO remove after everything works. This is only for mocking purposes.
-        val waitTimeBetweenRetries: Long = config.waitTimeBetweenRetries
-        println("Connection to config - should be (300000), is: $waitTimeBetweenRetries")
     }
 
 }
